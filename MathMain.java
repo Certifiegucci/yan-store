4295dcb40b9dcacc81c9194be18ba5b03d3a3bf2
Emmanuel Bony <certifieegucci@gmail.com>
2026-06-26 16:25:43 +0000

    ajout

--- MathMain.java
+++
@@ -1,6 +0,0 @@
-public class MathMain {
-    public static void main(String[] args) {
-        System.out.println("pi = " + Math.PI);
-        System.out.println("90 degres = " + Math.toRadians(90));
-    }
-}
